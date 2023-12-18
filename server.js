878eb9f1a523433eb4af8991276a3cd0256a48d7
Eduardo Lima Lacava <86937690+LacavaSP@users.noreply.github.com>
2023-12-17 22:33:57 -0300

    web

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const WebSocket = require('ws')
 const webSocketServidor = new WebSocket.Server({ port: 5555 })
 const pessoasConectadas = []
+const express = require('express')
+const cors = require('cors')
+const porta = 2525
+const servidor = express()
+servidor.use(cors({
+    allowedHeaders: "*",
+    origin: "*"
+}))
+servidor.use(express.json())
+servidor.use(express.static('public'))
 
 webSocketServidor.on('listening', (arg) => console.log('SERVIDOR WEBSOCKET ATIVO'))
 
@@ -21,3 +31,12 @@ webSocketServidor.on('connection', (socket) => {
         console.log('Cliente desconectado.');
     });
 });
+
+servidor.listen(porta, '195.35.37.40',() => {
+    console.log(`http://195.35.37.40:${porta}`)
+})
+
+servidor.post('/api/mensagem', (req, res) => {
+    pessoasConectadas.forEach((socket) => socket.send(JSON.stringify(req.body.msg)))
+    return res.status(200).json(req.body)
+})
